90e19e625849eb6d1deb638241c88ab696d5b9ee
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-02-20 13:00:17 -0800

    Slow video clip to audio duration

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+
+def get_length(input_video):
+    result = subprocess.run(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            input_video,
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    return float(result.stdout)
+
+
+def slow_video(adj, input_vid):
+    os.system(
+        "ffmpeg -i " + input_vid + ' -filter:v "setpts=' + str(adj) + '*PTS" output.mov'
+    )
+
+
+def get_slow_adj(audio_len, vid_len):
+    return audio_len / vid_len
+
+
+adj_amt = get_slow_adj(get_length("french.mp3"), get_length("input.mov"))
+print(adj_amt)
+slow_video(adj_amt, "input.mov")
+print("Done.")
